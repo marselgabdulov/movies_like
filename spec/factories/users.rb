e8ede49fc2104fe0b30d8b67d32f3062c67fe956
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-08-18 12:36:56 +0300

    create devise user

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  sequence :email do |n|
+    "user#{n}@test.com"
+  end
+
+  factory :user do
+    email
+    password { '12345678' }
+    password_confirmation { '12345678' }
+
+    after(:create) { |user| user.confirm }
+
+    trait :unconfirmed do
+      confirmed { false }
+    end
+  end
+end
